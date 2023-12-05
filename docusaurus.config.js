713e828679ac178c9639de62f8db3a9e960b29b6
Aaron Shekey <aaron@aaronshekey.com>
2023-12-05 17:32:56 -0600

    Restore the hubspot plugin

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,6 +35,7 @@ const config = {
 
 	plugins: [
 		"./src/plugins/ngrok-parse-integrations",
+		"@stackql/docusaurus-plugin-hubspot",
 		"@docusaurus/theme-mermaid",
 		"./src/plugins/tailwindcss",
 	],
